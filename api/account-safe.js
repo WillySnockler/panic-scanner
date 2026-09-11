a3e727e8518e233bda93a20f35518bd5ebf3bfc1
WillySnockler <johansenw84@gmail.com>
2026-09-11 19:11:53 +0200

    Return successful status for resilient account reads

--- api/account-safe.js
+++ api/account-safe.js
@@ -4,6 +4,7 @@ module.exports=async function(req,res){
   res.status=function(n){code=n;return res};
   res.json=function(body){
     if(code===503&&req.method==='GET'){
+      status(200);
       return json({profile:{plan:'Standard',is_admin:false,is_vip:false},settings:{sound:true,haptic:true,reduce_motion:false,volume:70,research_range:'60',compact_mode:false,chart_detail:2},watchlist:[],setups:[],investigations:[],theses:[],journal:[]});
     }
     return json(body);
